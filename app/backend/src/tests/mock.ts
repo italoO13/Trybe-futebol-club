7587bf29cb69342a26358c3522145e6fbd2db07c
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-29 13:05:00 -0300

    feat: mocks de loginsucess e user

---
+++ app/backend/src/tests/mock.ts
@@ -0,0 +1,13 @@
+export default {
+  loginSucess: {
+    email: 'teste@teste.com',
+    password: '123456'
+  },
+  user: {
+    id: 1,
+    username:'teste',
+    role: 'teste',
+    email: 'teste@teste.com',
+    password: '123456'
+  }
+}
