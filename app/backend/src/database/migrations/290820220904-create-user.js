ecce55f26555757fcf0fe4e099fd5aee18d5f624
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-29 09:59:10 -0300

    fix: erro ao definit autoIncrement em id do user

--- app/backend/src/database/migrations/290820220904-create-user.js
+++ app/backend/src/database/migrations/290820220904-create-user.js
@@ -4,7 +4,7 @@ module.exports = {
       id: {
         primaryKey: true,
         type: Sequelize.INTEGER,
-        autoincrement:true,
+        autoIncrement:true,
       },
       username: {
         allowNull:false,
