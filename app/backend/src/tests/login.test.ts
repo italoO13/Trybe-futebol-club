aec514f90e9325bf64cd86a71c6a0bd5c94e3015
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-29 13:06:32 -0300

    test: teste de integração que valida se usuário consegue logar com sucesso

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,43 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import mock from './mock'
+import { Response } from 'superagent';
+import IUserModel from '../model/ IUserModel';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testa a camada  de Login', () => {
+
+  let chaiHttpResponse: Response;
+  let userModel:IUserModel;
+
+  before(async () => {
+    userModel= {
+      findOne: sinon
+      .stub()
+      .resolves(mock.user)
+    }
+  });
+
+  after(()=>{
+    (userModel.findOne as sinon.SinonStub).restore();
+  })
+
+  describe('/POST', () => {
+    it('Deve cadastrar um usuário com sucesso', async() => {
+      const result = await chai.request(app).post('/login').send(mock.loginSucess);
+      expect(result.status).to.equal(201);
+      expect(result.body).to.be.property('token');
+    });
+
+
+  })
+
+});
