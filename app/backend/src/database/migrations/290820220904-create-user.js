d4abab856250b7bd7e302736c77c6795febeb1c0
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-29 09:23:56 -0300

    feat: migration users

---
+++ app/backend/src/database/migrations/290820220904-create-user.js
@@ -0,0 +1,30 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoincrement:true,
+      },
+      username: {
+        allowNull:false,
+        type: Sequelize.STRING,
+      },
+      role: {
+        allowNull:false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull:false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull:false,
+        type: Sequelize.STRING,
+      }
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  },
+};
