b99c241a50120146777b3586278911ca7e771d6d
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-29 09:24:07 -0300

    feat: model user

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,43 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement:true,
+    primaryKey:true
+  },
+  username: {
+    type:STRING,
+    allowNull:false
+  },
+  role: {
+    type:STRING,
+    allowNull:false
+  },
+  email: {
+    type:STRING,
+    allowNull:false
+  },
+  password: {
+    type:STRING,
+    allowNull:false
+  },
+
+}, {
+  underscored: true,
+  sequelize: db,
+  tableName:'users',
+  timestamps: false,
+});
+
+export default User;
