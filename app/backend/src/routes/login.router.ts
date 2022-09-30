ec05e1dc5a2e3a2d0171d0dacf3c821cbc771930
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-30 17:16:38 -0300

    refactor: adicionando o metodo get na rota login/validate

--- app/backend/src/routes/login.router.ts
+++ app/backend/src/routes/login.router.ts
@@ -3,13 +3,16 @@ import LoginService from "../services/LoginService";
 import LoginController from "../controllers/LoginController";
 import LoginModel from "../Repository/LoginModel";
 import LoginMiddleware from "../middlewares/login.middleware";
+import AuthMiddleware from '../middlewares/auth.middleware';
 
 const router = Router();
 const loginModel = new LoginModel()
 const loginService = new LoginService(loginModel)
 const loginController = new LoginController(loginService);
 const loginMiddleware = new LoginMiddleware();
+const authMiddleware = new AuthMiddleware();
 
 router.post('/',loginMiddleware.validateLogin, loginController.createSession)
+router.get('/validate', authMiddleware.authUser, loginController.validateAuthorization)
 
 export default router;
