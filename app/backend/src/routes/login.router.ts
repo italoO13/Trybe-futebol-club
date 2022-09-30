2a5f28115a35a9719ad6aca15aca21eafe283c85
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-30 15:36:50 -0300

    feat: adicionando a rota post de login o middleware de validação

--- app/backend/src/routes/login.router.ts
+++ app/backend/src/routes/login.router.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import LoginService from "../services/LoginService";
 import LoginController from "../controllers/LoginController";
 import LoginModel from "../Repository/LoginModel";
+import LoginMiddleware from "../middlewares/login.middleware";
 
 const router = Router();
 const loginModel = new LoginModel()
 const loginService = new LoginService(loginModel)
 const loginController = new LoginController(loginService);
+const loginMiddleware = new LoginMiddleware();
 
-router.post('/', loginController.createSession)
+router.post('/',loginMiddleware.validateLogin, loginController.createSession)
 
 export default router;
