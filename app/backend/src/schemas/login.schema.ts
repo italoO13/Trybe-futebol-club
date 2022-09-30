780f8ef8fb15676dd81f248d8d395ba53e18a9f5
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-30 15:55:44 -0300

    refactor: adicionado ao schema de login um validador de email

--- app/backend/src/schemas/login.schema.ts
+++ app/backend/src/schemas/login.schema.ts
@@ -1,9 +1,10 @@
 import* as Joi from 'joi'
 
 const InputLogin = Joi.object({
-  email: Joi.string().required().messages({
+  email: Joi.string().email().required().messages({
     'string.empty': '400|All fields must be filled',
-    'string.required': '400|All fields must be filled'
+    'string.required': '400|All fields must be filled',
+    'string.email':'401|Incorrect email or password'
   }),
   password: Joi.string().required().messages({
     'string.empty': '400|All fields must be filled',
