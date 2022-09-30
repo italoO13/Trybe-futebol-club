77687bbf3a68b593f5081e92d26a1f182b911e7c
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-30 15:37:04 -0300

    refactor: alterando alguns dados mockados

--- app/backend/src/tests/mock.ts
+++ app/backend/src/tests/mock.ts
@@ -1,13 +1,13 @@
 export default {
   loginSucess: {
-    email: 'teste@teste.com',
-    password: '123456'
+    email: "admin@admin.com",
+    password: "secret_admin"
   },
   user: {
     id: 1,
     username:'teste',
     role: 'teste',
     email: 'teste@teste.com',
-    password: '123456'
+    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
   }
 }
