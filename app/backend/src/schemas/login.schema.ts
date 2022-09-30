dd1eda7ad4ea8dff5e1138edc4d9c9733aeec55d
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-30 15:49:03 -0300

    refactor: adicionado ao schema de login a validação para password

--- app/backend/src/schemas/login.schema.ts
+++ app/backend/src/schemas/login.schema.ts
@@ -5,7 +5,10 @@ const InputLogin = Joi.object({
     'string.empty': '400|All fields must be filled',
     'string.required': '400|All fields must be filled'
   }),
-  password: Joi.string()
+  password: Joi.string().required().messages({
+    'string.empty': '400|All fields must be filled',
+    'string.required': '400|All fields must be filled'
+  })
 })
 
 export default InputLogin;
