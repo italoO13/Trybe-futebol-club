13c74a5d730d27a7eee2110cdd047e23fee03d1c
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-30 17:15:46 -0300

    refactor: metodo validateAuthorization que retorna na rota get informações de role

--- app/backend/src/controllers/ILoginController.ts
+++ app/backend/src/controllers/ILoginController.ts
@@ -1,5 +1,7 @@
 import { Response, Request, NextFunction } from 'express';
+import IRequestWithUser from '../interfaces/IRequestWithUser';
 
 export default interface ILoginController {
-  createSession(req:Request, res:Response, next: NextFunction):void
+  createSession(req:Request, res:Response, next: NextFunction):void;
+  validateAuthorization(req:IRequestWithUser, res:Response, next: NextFunction):void
 }
--- app/backend/src/controllers/LoginController.ts
+++ app/backend/src/controllers/LoginController.ts
@@ -1,4 +1,6 @@
 import { Response, Request, NextFunction } from 'express';
+import IJWT from '../interfaces/IAuth';
+import IRequestWithUser from '../interfaces/IRequestWithUser';
 import ILoginService from '../services/ILoginService';
 import ILoginController from './ILoginController';
 
@@ -14,6 +16,15 @@ export default class LoginController implements ILoginController {
       const {email, password} = req.body;
       const result = await this.service.createSession({email, password})
       res.status(200).json({token: result})
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  validateAuthorization = async(req: IRequestWithUser, res:Response, next:NextFunction) => {
+    try {
+      const {role} = req.user as IJWT;
+      res.status(200).json({role})
     } catch (error) {
       next(error);
     }
