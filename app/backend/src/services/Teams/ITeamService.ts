029d7eb5da72bde1b2c0dcc486a9684cd7f7231f
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-01 12:21:12 -0300

    feat: interface e implementação da camada de service  para o metodo getById em teams

--- app/backend/src/services/Teams/ITeamService.ts
+++ app/backend/src/services/Teams/ITeamService.ts
@@ -2,4 +2,5 @@ import ITeam from "../../interfaces/ITeam";
 
 export default interface ITeamsService {
   getAll():Promise<ITeam[]>
+  getById(id:number):Promise<ITeam>
 }
--- app/backend/src/services/Teams/TeamService.ts
+++ app/backend/src/services/Teams/TeamService.ts
@@ -1,12 +1,21 @@
 import ITeam from "../../interfaces/ITeam";
 import ITeamsService from "./ITeamService";
 import ITeamsModel from "../../Repository/Teams/ITeamsModel";
+import CustomError from "../../helper/CustomError";
 
 export default class TeamsService implements ITeamsService {
   private model: ITeamsModel;
 
   constructor(model: ITeamsModel){
     this.model = model;
+  }
+
+  async getById(id: number): Promise<ITeam> {
+    const result = await this.model.getById(id);
+    if(!result) {
+      throw new CustomError(400, 'Team not found')
+    }
+    return result;
   }
 
   async getAll(): Promise<ITeam[]> {
