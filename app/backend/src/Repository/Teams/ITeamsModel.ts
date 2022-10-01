5c4cabd7d932d28074db7d30ab8a6cf91ba5c5e8
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-01 12:21:00 -0300

    feat: interface e implementação da camada de model  para o metodo getById em teams

--- app/backend/src/Repository/Teams/ITeamsModel.ts
+++ app/backend/src/Repository/Teams/ITeamsModel.ts
@@ -2,4 +2,5 @@ import ITeam from "../../interfaces/ITeam";
 
 export default interface ITeamsModel {
   getAll():Promise<ITeam[]>
+  getById(id:number):Promise<ITeam | null>
 }
--- app/backend/src/Repository/Teams/TeamsModel.ts
+++ app/backend/src/Repository/Teams/TeamsModel.ts
@@ -4,6 +4,11 @@ import Team from "../../database/models/Team";
 
 export default class TeamsModel implements ITeamsModel {
   model = Team;
+  
+  async getById(id:number): Promise<ITeam | null> {
+    const result = await this.model.findByPk(id)
+    return result;
+  }
 
   async getAll(): Promise<ITeam[]> {
     const result = await this.model.findAll()
