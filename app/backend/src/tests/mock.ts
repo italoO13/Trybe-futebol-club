28a1961d770622ef6b1ae7dfbe880b2b49075c12
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-03 10:28:44 -0300

    chore: mock de matches

--- app/backend/src/tests/mock.ts
+++ app/backend/src/tests/mock.ts
@@ -24,5 +24,35 @@ export default {
       "id": 3,
       "teamName": "Botafogo"
     },
+  ],
+  matches: [
+    {
+      "id": 1,
+      "homeTeam": 16,
+      "homeTeamGoals": 1,
+      "awayTeam": 8,
+      "awayTeamGoals": 1,
+      "inProgress": false,
+      "teamhome": {
+        "teamName": "São Paulo"
+      },
+      "teamAway": {
+        "teamName": "Grêmio"
+      }
+    },
+    {
+      "id": 2,
+      "homeTeam": 9,
+      "homeTeamGoals": 1,
+      "awayTeam": 14,
+      "awayTeamGoals": 1,
+      "inProgress": false,
+      "teamhome": {
+        "teamName": "Internacional"
+      },
+      "teamAway": {
+        "teamName": "Santos"
+      }
+    },
   ]
 }
