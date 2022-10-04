51acbfaf311f2eb6c68d04ef71758af2acfa1eae
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-04 14:55:44 -0300

    feat: interface que representa um Leadboard

---
+++ app/backend/src/interfaces/ILeaderBoard.ts
@@ -0,0 +1,12 @@
+export default interface ILeaderBoard {
+  name?: string,
+  totalGames: number,
+  totalVictories:number,
+  totalPoints: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalance:number,
+  efficiency: number,
+}
