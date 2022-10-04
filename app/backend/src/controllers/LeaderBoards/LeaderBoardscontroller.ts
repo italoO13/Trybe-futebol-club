32b43997446627d782907ee6fc429ee2c6d4f6b2
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-04 14:55:19 -0300

    feat: camada de controller de leaderbord que retorna a classificação dos times em casa

---
+++ app/backend/src/controllers/LeaderBoards/LeaderBoardscontroller.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import IleaderBoardsController from "./ILeaderBoardsController";
+import ILeaderBoardsService from "../../services/LeaderBoards/ILeaderBoardsService";
+
+export default class MatchersController implements IleaderBoardsController {
+  service: ILeaderBoardsService;
+  constructor(service: ILeaderBoardsService){
+    this.service = service;
+  }
+  getAllHome = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+      const response = await this.service.getAllHome();
+      res.status(200).json(response);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
